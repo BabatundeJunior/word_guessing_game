2a7d83c7b90e333652b03c243afb88e4188ba16a
Babatunde Junior <jbabs128@gmail.com>
2024-10-09 17:30:23 +0100

    Update Wordguesinggame.py

--- Wordguesinggame.py
+++ Wordguesinggame.py
@@ -1,8 +1,7 @@
 """
-I added creativity of randomly selecting the secret word for the user, 
-by adding a list for words to randomly select from. By importing random to do the selection
+I added the creativity of randomly selecting the secret word for the user, 
+by adding a list of words to choose from randomly. By importing random to do the selection
 """
-print("Welcome to the Word Guessing Game!")
 import random
 words = ["coding", "python", "code", "loop", "program"]
 secret = random.choice(words)
@@ -13,7 +12,7 @@ count = 0
 
 #Using a loop to ask the user to guess the secret word till they get it correctly
 while True:
-    guess = input("Guess the word: ")
+    guess = input("Guess the word according to the number of spaces provided: ")
     count += 1
     if guess == secret:
         print("Great Job! You guessed it!")
@@ -26,7 +25,7 @@ while True:
         print("Too long! it must have the same number of letters as the secret word!")
         continue
 
-    # added hint functanality to give hints to the user
+    # Added hint functionality to give hints to the user
     hint = []
     for letter in range(len(secret)):
         if guess[letter] == secret[letter]:
@@ -35,14 +34,10 @@ while True:
             hint.append(guess[letter].lower())
         else:
             hint.append("_")
-    print(f"A little hint for you {" ".join(hint)}")
+    print(f"A little hint for you  { " ".join(hint)}")
     print(
         """
-        small letters show the letters are in the secrete word while capital 
-        letters show that is the position of the letter in the secret word.
+        UPPERCASE letters mean the letter is correct and in the right position.
+        lowercase letters mean the letter is in the word but in the wrong position.
         """
         )
-
-
-
-    
